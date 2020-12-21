95c4467a689f9bf9643eef75d43a0697fc90be28
dubwe <emmanuelwilliams515@gmail.com>
2020-12-21 17:23:28 +0200

    added Textfields and a button

---
+++ Loan.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.geometry.HPos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+public class Loan extends Application{
+	private TextField maizeSize = new TextField();
+	private TextField soyaSize = new TextField();
+	private TextField groundnutsSize = new TextField();
+	private TextField saltSize = new TextField();
+	private TextField  = new TextField();
+
+
+
+
+}
