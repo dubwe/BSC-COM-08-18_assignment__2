55d491df6aa622d8867bf04b7fed25de8075bf77
dubwe <emmanuelwilliams515@gmail.com>
2020-12-23 01:14:37 +0200

    trying to allign the labels and textfields

--- Loan.java
+++ Loan.java
@@ -10,11 +10,44 @@ import javafx.stage.Stage;
 
 
 public class Loan extends Application{
+	//creating textfields areas to input the values for various crops 
 	private TextField maizeSize = new TextField();
 	private TextField soyaSize = new TextField();
 	private TextField groundnutsSize = new TextField();
 	private TextField saltSize = new TextField();
-	private TextField  = new TextField();
+	private TextField calculate = new TextField();
+	//creating a button that will calculate 
+	private Button btcalculate = new Button("Calculate");
+
+
+	@Override
+	public void start(Stage primaryStage){
+
+		GridPane gridPane = new GridPane();
+		gridPane.setHgap(4);
+		gridPane.setHgap(4);
+
+		gridPane.add(new Label("Maize"),1,0);
+		gridPane.add(maizeSize,4,0);
+
+		gridPane.add(new Label("Soya:"),2,1);
+ 		gridPane.add(soyaSize,4,1);
+
+ 		gridPane.add(new Label("Groundnuts:"),0,3);
+		//gridPane.add(soyaSize,2,3);
+
+
+		//gridPane.add(new Label("Salt"),0,4);
+		//gridPane.add(saltSize,3,4);
+
+		Scene scene = new Scene(gridPane,500,500);
+		primaryStage.setTitle("Feed Calculator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+	}
+
 
 
 
