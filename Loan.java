43b92f6cd2bc075e81c45404756258260088be83
dubwe <emmanuelwilliams515@gmail.com>
2020-12-23 11:40:18 +0200

    Trying out a new pane

--- Loan.java
+++ Loan.java
@@ -5,8 +5,15 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.geometry.Insets;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 
 
 public class Loan extends Application{
@@ -22,25 +29,51 @@ public class Loan extends Application{
 
 	@Override
 	public void start(Stage primaryStage){
+		
 
 		GridPane gridPane = new GridPane();
+		gridPane.setVgap(10);
 		gridPane.setHgap(4);
-		gridPane.setHgap(4);
 
-		gridPane.add(new Label("Maize"),1,0);
-		gridPane.add(maizeSize,4,0);
+		//Creating a Text object 
+		Text instruction = new Text("Enter ingredient amounts in Kilograms (kg)");
+		HBox topBox = new HBox(instruction);
+		topBox.setPadding(new Insets(0,0,0,0));
+		topBox.setAlignment(Pos.CENTER);
 
-		gridPane.add(new Label("Soya:"),2,1);
- 		gridPane.add(soyaSize,4,1);
+		Label maizeLabel = new Label("Maize");
+		maizeLabel.setPrefWidth(120);
+		TextField maizeTextField = new TextField();
+		maizeLabel.setTextAlignment(TextAlignment.RIGHT);
+		maizeTextField.setPrefColumnCount(10);
+		maizeTextField.setPrefWidth(40);
+		HBox maizeBox = new HBox(maizeLabel, maizeTextField);
+      	
 
- 		gridPane.add(new Label("Groundnuts:"),0,3);
+      	//gridPane.add(text,7,0);
+		
+
+		gridPane.add(new Label("Maize"),7,2);
+		//gridPane.add(maizeSize,4,0);
+
+		//gridPane.add(new Label("Soya:"),2,1);
+ 		//gridPane.add(soyaSize,4,1);
+
+ 		//gridPane.add(new Label("Groundnuts:"),0,3);
 		//gridPane.add(soyaSize,2,3);
 
 
 		//gridPane.add(new Label("Salt"),0,4);
 		//gridPane.add(saltSize,3,4);
 
-		Scene scene = new Scene(gridPane,500,500);
+
+		
+ 	  
+		//BorderPane bpane = new BorderPane();
+		//bpane.setCenter(gridPane);
+		//bpane.setPadding(new Insets(11.5,12.5,13.5,14.5));
+        VBox vBox = new VBox(topBox, maizeBox);
+		Scene scene = new Scene(vBox,500,500);
 		primaryStage.setTitle("Feed Calculator");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -48,8 +81,12 @@ public class Loan extends Application{
 
 	}
 
+}
 
-
-
-
+class CustomePane {
+	 public void CustomPane(String text) {
+	 	System.out.println("hie");
+		 //getChildren().add(new Label(text)); 
+		 //setPadding(new Insets(11.5,12.5,13.5,14.5));
+	}
 }
